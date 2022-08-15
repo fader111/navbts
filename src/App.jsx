f858fcf5184c76e8ff3adbfd0a6a2a0b464ae757
Andrey Taranov <andrey.taranov@ormco.com>
2022-08-15 19:18:06 +0300

    first commin

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+// import logo from './logo.svg';
+// import './App.css';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Menu from "./Menu";
+
+
+const App = () => {
+  return (
+
+      <Menu />
+    );
+  }
+
+export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+// import { Switch, Route, Redirect } from "react-router-dom";
+// import logo from './logo.svg';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Menu() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Дорожный контроллер</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#main">Главная</Nav.Link>
+            <Nav.Link href="#logs">Логи</Nav.Link>
+            <NavDropdown title="Настройки" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Настройки</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#program">Программа</Nav.Link>
+            <Nav.Link eventKey={2} href="#firmware">
+              Прошивка
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
