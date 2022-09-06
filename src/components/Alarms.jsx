c233a31d694f9bbbaf2351162c640fbaa4ad9dc1
Andrey Taranov <andrey.taranov@ormco.com>
2022-09-06 22:42:39 +0300

    minor changes

--- src/components/Alarms.jsx
+++ src/components/Alarms.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Alarms = () => {
     return (
-      <>
-        <div> Аварии текст тут быть должен</div>
-    </>
+        <>
+            <div> Аварии текст тут быть должен</div>
+        </>
     );
 };
 export default Alarms;
