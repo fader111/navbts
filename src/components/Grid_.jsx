15955536fbc4059ace117ff6d9acaf6c404c292f
fader111 <fader111@yandex.ru>
2022-09-11 19:43:29 +0300

    stash

--- src/components/Grid_.jsx
+++ src/components/Grid_.jsx
@@ -1,13 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Table1 from "./Table1";
+import Table2 from "./Table2";
 
 function Grid1() {
   return (
     <Container>
       <Row>
-        <Col sm={8}>sm=8</Col>
-        <Col sm={4}>sm=4</Col>
+        <Col sm>abra</Col>
+        <Col sm>caaadabra</Col>
+        {/* {Table1} */}
+        {/* {Table2} */}
       </Row>
       <Row>
         <Col sm>sm=true</Col>
